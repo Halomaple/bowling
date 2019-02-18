da647d9f425081f919913f727bcf8c7a511b009f
Jeff <jeff.doyle@foxmail.com>
2019-02-18 16:29:06 +0800

    add implementation

--- src/app/scoring/scoring.component.spec.ts
+++ src/app/scoring/scoring.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScoringComponent } from './scoring.component';
 
-fdescribe('ScoringComponent', () => {
+describe('ScoringComponent', () => {
 	let game: ScoringComponent;
 	let fixture: ComponentFixture<ScoringComponent>;
 
@@ -22,28 +22,152 @@ fdescribe('ScoringComponent', () => {
 		expect(game).toBeTruthy();
 	});
 
-	it('should score a gutter', () => {
+	it('should score a gutter game', () => {
 		game.Roll(0);
 		game.Roll(0);
+
 		game.Roll(0);
 		game.Roll(0);
+
 		game.Roll(0);
 		game.Roll(0);
+
 		game.Roll(0);
 		game.Roll(0);
+
 		game.Roll(0);
 		game.Roll(0);
+
 		game.Roll(0);
 		game.Roll(0);
+
 		game.Roll(0);
 		game.Roll(0);
+
 		game.Roll(0);
 		game.Roll(0);
+
 		game.Roll(0);
 		game.Roll(0);
+
 		game.Roll(0);
 		game.Roll(0);
 
 		expect(game.Score()).toBe(0);
+	});
+
+	it('should score one roll', () => {
+		game.Roll(1);
+
+		expect(game.Score()).toBe(1);
+	});
+
+	it('should score one strike', () => {
+		game.Roll(10);
+
+		expect(game.Score()).toBe(10);
+	});
+
+	it('should score one strike and two rolls', () => {
+		game.Roll(10);
+
+		game.Roll(1);
+		game.Roll(2);
+
+		expect(game.Score()).toBe(16);
+	});
+
+	it('should score two strikes and next two rolls', () => {
+		game.Roll(10);
+
+		game.Roll(10);
+
+		game.Roll(2);
+		game.Roll(2);
+
+		expect(game.Score()).toBe(40);
+	});
+
+	it('should score a spare', () => {
+		game.Roll(5);
+		game.Roll(5);
+
+		game.Roll(1);
+
+		expect(game.Score()).toBe(12);
+	});
+
+	it('should score two spares', () => {
+		game.Roll(5);
+		game.Roll(5);
+
+		game.Roll(6);
+		game.Roll(4);
+
+		game.Roll(1);
+
+		expect(game.Score()).toBe(28);
+	});
+
+	it('should score last spare', () => {
+		game.Roll(1);
+		game.Roll(1);
+
+		game.Roll(1);
+		game.Roll(1);
+
+		game.Roll(1);
+		game.Roll(1);
+
+		game.Roll(1);
+		game.Roll(1);
+
+		game.Roll(1);
+		game.Roll(1);
+
+		game.Roll(1);
+		game.Roll(1);
+
+		game.Roll(1);
+		game.Roll(1);
+
+		game.Roll(1);
+		game.Roll(1);
+
+		game.Roll(1);
+		game.Roll(1);
+
+		game.Roll(5);
+		game.Roll(5);
+
+		game.Roll(1);
+
+		expect(game.Score()).toBe(29);
+	});
+
+	it('should score a perfect game', () => {
+		game.Roll(10);
+
+		game.Roll(10);
+
+		game.Roll(10);
+
+		game.Roll(10);
+
+		game.Roll(10);
+
+		game.Roll(10);
+
+		game.Roll(10);
+
+		game.Roll(10);
+
+		game.Roll(10);
+
+		game.Roll(10);
+
+		game.Roll(10);
+
+		expect(game.Score()).toBe(300);
 	});
 });
--- src/app/scoring/scoring.component.ts
+++ src/app/scoring/scoring.component.ts
@@ -6,7 +6,55 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./scoring.component.scss']
 })
 export class ScoringComponent implements OnInit {
+	private rolls = new Array(21);
+	private rollIndex = 0;
+
 	constructor() {}
 
-	ngOnInit() {}
+	ngOnInit() {
+		for (let i = 0; i < this.rolls.length; i++) {
+			this.rolls[i] = 0;
+		}
+	}
+
+	Score(): number {
+		let score = 0;
+		let currentFrame = 0;
+		for (let frame = 0; frame < 10; frame++) {
+			currentFrame = frame * 2;
+			const nextFrameIndex = (frame + 1) * 2;
+
+			// score current frame first
+			score += this.rolls[currentFrame] + this.rolls[currentFrame + 1];
+
+			// and then continue with strike or spare
+			if (this.rolls[currentFrame] === 10) {
+				// strike!
+				if (this.rolls[nextFrameIndex] === 10) {
+					// strike again!
+					score += this.rolls[nextFrameIndex] + this.rolls[frame < 9 ? nextFrameIndex + 2 : nextFrameIndex];
+				} else {
+					score += this.rolls[nextFrameIndex] + this.rolls[nextFrameIndex + 1];
+				}
+			} else if (this.rolls[currentFrame] + this.rolls[currentFrame + 1] === 10) {
+				// spare!
+				score += this.rolls[nextFrameIndex];
+			}
+		}
+
+		return score;
+	}
+
+	Roll(rollNumber: number) {
+		if (rollNumber > 10) {
+			throw new Error('rollNumber can not be greater than 10!');
+		}
+
+		this.rolls[this.rollIndex] = rollNumber;
+		if (rollNumber === 10) {
+			this.rollIndex += 2;
+		} else {
+			this.rollIndex++;
+		}
+	}
 }
